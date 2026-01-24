8b09979183a8d11a40afbe2939c2c7f782007d7a
Abiel Almonte <almonteabiel@gmail.com>
2026-01-24 04:10:58 -0500

    the compiler fundamentallly is for cuda to add it to the cuda version of torq

--- torq/__init__.py
+++ torq/__init__.py
@@ -5,17 +5,17 @@ from .core import (
     register_pipe as register_pipe,
     register_pipe_decorator as register_pipe_decorator,
 )
-from .compiler import (
-    register_backend as register_backend,
-    compile as compile,
-    _fulqrum as _fulqrum,
-)
-
 
 HAS_CUDA = False
 
 try:
     from . import cuda as cuda
+
+    from .compiler import (
+        register_backend as register_backend,
+        compile as compile,
+        _fulqrum as _fulqrum,
+    )
 
     HAS_CUDA = True
 except ImportError as e:
