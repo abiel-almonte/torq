d08338063ab39bf6a38fbc601636647e92f70778
Abiel Almonte <almonteabiel@gmail.com>
2026-01-24 00:34:16 -0500

    Use as X pattern to re export modules as public

--- torq/__init__.py
+++ torq/__init__.py
@@ -1,10 +1,21 @@
-from .core import Pipe, Sequential, Concurrent, register_pipe, register_pipe_decorator
-from .compiler import register_backend, compile, _fulqrum
+from .core import (
+    Pipe as Pipe,
+    Sequential as Sequential,
+    Concurrent as Concurrent,
+    register_pipe as register_pipe,
+    register_pipe_decorator as register_pipe_decorator,
+)
+from .compiler import (
+    register_backend as register_backend,
+    compile as compile,
+    _fulqrum as _fulqrum,
+)
+
 
 HAS_CUDA = False
 
 try:
-    from . import cuda
+    from . import cuda as cuda
 
     HAS_CUDA = True
 except ImportError as e:
