7b5913c5da9eab348c0f76c587028d219cf92106
Abiel Almonte <almonteabiel@gmail.com>
2026-02-23 20:24:12 +0000

    Update setup.py to use CUDA_HOME environment variable for include and library directories

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ import os
 
 ext_modules = []
 
+cuda_home = os.environ.get("CUDA_HOME", "/usr/local/cuda")
+
 if os.environ.get("TORQ_CUDA", "1") == "1":
     ctorq_extension = Extension(
         name="torq.cuda._C",
         sources=glob.glob("csrc/**/*.c", recursive=True),
-        include_dirs=["./csrc", "/usr/include/cuda/", "/usr/include/python3.12/"],
-        library_dirs=["/usr/local/cuda/lib64"],
+        include_dirs=["./csrc", os.path.join(cuda_home, "include"), "/usr/include/python3.12/"],
+        library_dirs=[os.path.join(cuda_home, "lib64")],
     )
     ext_modules.append(ctorq_extension)
 
